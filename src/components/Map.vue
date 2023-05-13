5ee447f8d42a52113620d27debd4760735b526cf
TheMagicApple <90540132+TheMagicApple@users.noreply.github.com>
2023-05-13 12:01:57 -0700

    BIG update 5

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,16 @@
 <template>
 	<div id="Background">
-		<img :src="require(`../assets/worldmap2.png`)" />
-		<Trash x="40%" y="20%"></Trash>
+		<img :src="require(`../assets/world3.svg`)" />
+		<div id="infofake">
+		<!--<div id="id"><b>TOTAL</b></div>
+		<div id="numfake"><b>100K</b></div>
+		<div id="numlabel">pieces of trash</div>
+		<div id="location">📍 300+ Trash Heaps 📍</div>
+		</div>
+		-->
+	</div>
+		<Trash x="30%" y="20%" s="20px" n="14" lat="22.3545" log="33.5634" nu="0" id="252583"></Trash>
+		<Trash x="30%" y="30%" s="30px" n="37" lat="22.3545" log="33.5634" nu="1" id="123456"></Trash>
 	</div>
 	
 </template>
@@ -18,9 +27,107 @@ export default{
 body{
 	
 }
+
+
+
+
+#infofake{
+	background-color:rgb(30,30,30);
+	position:absolute;
+	left:1690px;
+	width:200px;
+	top:3%;
+	height:30%;
+	border-radius:50px 50px 0px 0px;
+}
+#back{
+	background:transparent;
+	width:100%;
+	height:100%;
+	position:absolute;
+	left:0%;
+	top:0%;
+	
+}
+.invis{
+	opacity:0;
+}
+#id{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:8%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:25px;
+}
+#num{
+	font-size:60px;
+	position:absolute;
+	width:100px;
+	left:25%;
+	top:25%;
+	height:100px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:rgb(240, 67, 67);
+	border-radius:50px;
+	border:3px dashed rgb(240, 67, 67);
+}
+#numfake{
+	font-size:50px;
+	position:absolute;
+	width:150px;
+	left:12%;
+	top:25%;
+	height:100px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:rgb(240, 67, 67);
+	border-radius:50px;
+	border:3px dashed rgb(240, 67, 67);
+}
+#numlabel{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:65%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:20px;
+}
+#location{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:80%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:17px;
+}
+
+
+
+
 #Background{
 	overflow:hidden;
-	background-color:rgb(50,50,50);
+	background-color:rgb(235,235,235);
 	position:absolute;
 	left:0%;
 	top:0%;
@@ -32,5 +139,6 @@ img{
 	height:980px;
 	position:absolute;
 	filter:brightness(120%);
+	left:30px;
 }
 </style>
--- src/components/Trash.vue
+++ src/components/Trash.vue
@@ -1,38 +1,195 @@
 <template>
-	<div id="outer" :style="css"></div>
+	<!--<div id="back" v-on:click="hide"></div>-->
+	<div v-on:click="reveal" id="outer" :style="css"></div>
 	
+	<div class="info invis" >
+		<div id="id"><b>ID #{{id}}</b></div>
+		<div id="num"><b>{{n}}</b></div>
+		<div id="numlabel">pieces of trash</div>
+		<div id="location">📍 {{lat}} {{log}} 📍</div>
+		<button id="current"><b>Current</b></button>
+		<button id="week"><b>7 Days</b></button>
+	</div>
 </template>
 <script>
 export default{
-	props:["x","y"],
+	props:["x","y","s","n","lat","log","nu","id"],
 	computed:{
 		css(){
 			return{
 				"--x":this.x,
 				"--y":this.y,
+				"--s":this.s,
 			}
 		}
 	},
+	methods:{
+		reveal:function(){
+			var elements=document.getElementsByClassName("info");
+			for(var i=0;i<elements.length;i++){
+				if(i==parseInt(this.nu)){
+					elements[i].classList.remove("invis");
+				}else{
+					elements[i].classList.add("invis");
+				}
+			}
+			//document.getElementsByClassName("info")[parseInt(this.num)].classList.remove("invis");
+		},
+		hide:function(){
+			document.getElementsByClassName("info")[parseInt(this.num)].classList.add("invis");
+		}
+	}
 }
 </script>
 <style scoped>
 #outer{
 	background-color:rgb(240, 67, 67);
-	width:20px;
-	height:20px;
-	border-radius:20px;
+	width:var(--s);
+	height:var(--s);
+	border-radius:1000px;
 	position:absolute;
 	left:var(--x);
 	top:var(--y);
+	border:calc(var(--s) / 10) solid rgb(50,50,50);
+	transition:0.2s all;
+}
+#outer:hover{
+	transform:scale(1.1);
+}
+.info{
+	background-color:rgb(30,30,30);
+	position:absolute;
+	left:1690px;
+	width:200px;
+	top:3%;
+	height:30%;
+	border-radius:50px 50px 0px 0px;
+	
+	transition:0.1s all;
+}
+#infofake{
+	background-color:rgb(30,30,30);
+	position:absolute;
+	left:1690px;
+	width:200px;
+	top:3%;
+	height:30%;
+	border-radius:50px 50px 0px 0px;
+}
+#back{
+	background:transparent;
+	width:100%;
+	height:100%;
+	position:absolute;
+	left:0%;
+	top:0%;
 	
 }
-#inner{
-	background-color:rgb(219, 0, 0);
-	width:15px;
-	height:15px;
-	border-radius:20px;
-	position:absolute;
-	left:calc(var(--x) + 2.5px);
-	top:calc(var(--y) + 2.5px);
+.invis{
+	opacity:0;
 }
+#id{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:8%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:25px;
+}
+#num{
+	font-size:60px;
+	position:absolute;
+	width:100px;
+	left:25%;
+	top:25%;
+	height:100px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:rgb(240, 67, 67);
+	border-radius:50px;
+	border:3px dashed rgb(240, 67, 67);
+}
+#numfake{
+	font-size:50px;
+	position:absolute;
+	width:150px;
+	left:12%;
+	top:25%;
+	height:100px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:rgb(240, 67, 67);
+	border-radius:50px;
+	border:3px dashed rgb(240, 67, 67);
+}
+#numlabel{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:65%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:20px;
+}
+#location{
+	position:absolute;
+	width:100%;
+	left:0%;
+	top:80%;
+	height:10%;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+	text-align:center;
+	color:white;
+	font-size:17px;
+}
+#current{
+	background-color:rgb(61, 131, 245);
+	color:white;
+	border:none;
+	outline:none;
+	position:absolute;
+	left:0%;
+	top:100%;
+	width:50%;
+	height:10%;
+	font-family: 'Open Sans', sans-serif;
+	border-radius:0px 0px 0px 20px;
+	display:flex;
+	justify-content:center;
+	align-items:center;
+}
+#week{
+	background-color:transparent;
+	color:white;
+	border:none;
+	outline:none;
+	position:absolute;
+	left:50%;
+	top:100%;
+	width:50%;
+	height:10%;
+	font-family: 'Open Sans', sans-serif;
+	color:rgb(61,131,245);
+	border-radius:0px 0px 20px 0px;
+	border:2px solid rgb(61,131,245);
+	display:flex;
+	justify-content:center;
+	align-items:center;
+}
+
 </style>
