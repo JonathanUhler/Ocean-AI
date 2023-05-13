74fb80f28032c38adab5bf15ce525e14c80b8d54
TheMagicApple <90540132+TheMagicApple@users.noreply.github.com>
2023-05-13 10:56:13 -0700

    update 4

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,31 @@
 <template>
-	<img :src="require(`../assets/worldmap2.png`)" />
+	<div id="Background">
+		<img :src="require(`../assets/worldmap2.png`)" />
+		<Trash x="40%" y="20%"></Trash>
+	</div>
+	
 </template>
 <script>
-
+import Trash from "./Trash.vue";
+export default{
+	components:{
+		Trash
+	},
+}
+	
 </script>
-<style>
+<style scoped>
 body{
+	
+}
+#Background{
 	overflow:hidden;
 	background-color:rgb(50,50,50);
+	position:absolute;
+	left:0%;
+	top:0%;
+	width:100%;
+	height:100%;
 }
 img{
 	width:1920px;
---
+++ src/components/Trash.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="outer" :style="css"></div>
+	
+</template>
+<script>
+export default{
+	props:["x","y"],
+	computed:{
+		css(){
+			return{
+				"--x":this.x,
+				"--y":this.y,
+			}
+		}
+	},
+}
+</script>
+<style scoped>
+#outer{
+	background-color:rgb(240, 67, 67);
+	width:20px;
+	height:20px;
+	border-radius:20px;
+	position:absolute;
+	left:var(--x);
+	top:var(--y);
+	
+}
+#inner{
+	background-color:rgb(219, 0, 0);
+	width:15px;
+	height:15px;
+	border-radius:20px;
+	position:absolute;
+	left:calc(var(--x) + 2.5px);
+	top:calc(var(--y) + 2.5px);
+}
+</style>
