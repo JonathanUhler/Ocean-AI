bfc84dd62214c980617fdffdbac45cbf1db53f8c
TheMagicApple <90540132+TheMagicApple@users.noreply.github.com>
2023-05-13 10:46:39 -0700

    update 3

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,9 +1,18 @@
 <template>
-map
+	<img :src="require(`../assets/worldmap2.png`)" />
 </template>
 <script>
 
 </script>
-<style scoped>
-
+<style>
+body{
+	overflow:hidden;
+	background-color:rgb(50,50,50);
+}
+img{
+	width:1920px;
+	height:980px;
+	position:absolute;
+	filter:brightness(120%);
+}
 </style>
